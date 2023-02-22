f33e8678e962d5f18165edfe130ccaac4f2bd775
Payal Sasmal <sasmalpayal@gmail.com>
2023-02-22 22:22:12 +0530

    redux is adding for buttons

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,57 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-
-
 const initialState = { showToggle: false };
 const cartSlice = createSlice({
-    name:'cart',
-    initialState,
-    reducers: {
-        cartToggle(state){
-            state.showToggle = !state.showToggle;
+  name: "cart",
+  initialState,
+  reducers: {
+    cartToggle(state) {
+      state.showToggle = !state.showToggle;
+    },
+  },
+});
+
+const cartItemsChangeSlice = createSlice({
+  name: "cartItems",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      }else{
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+        const id = action.payload;
+        const existingItem = state.items.find((item) => item.id === id);
+        if(existingItem.quantity === 1){
+            state.items = state.items.filter(item => item.id !== id)
+        }else{
+            existingItem.quantity--;
+            existingItem.totalPrice -= existingItem.price
         }
-    }
+    },
+  },
 });
 
 const store = configureStore({
-    reducer: {cart : cartSlice.reducer}
+  reducer: { cart: cartSlice.reducer, cartItemsChange: cartItemsChangeSlice},
 });
 
 export const cartActions = cartSlice.actions;
+export const cartItemChangeActions = cartItemsChangeSlice.actions;
 
 export default store;
